ce4c81b34a2b6dea4ab89b2388eb86b29b075cdc
Viktoriya <gonchikova_00@mail.ru>
2023-03-04 07:44:29 +0700

    -

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Authenticatable implements JWTSubject, AuthenticatableContract,AuthorizableContract
+class User extends Model implements JWTSubject, AuthenticatableContract,AuthorizableContract
 {
     use Authenticatable, Authorizable, HasFactory;
 
